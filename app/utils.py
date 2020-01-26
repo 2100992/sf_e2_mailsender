a52c80c67cc23f7d548389ca5dc233ffce1262c6
Shvd <2100992@gmail.com>
2020-01-27 02:40:44 +0300

    release ThreadSMTPSender

--- app/utils.py
+++ app/utils.py
@@ -1,18 +1,53 @@
 from threading import Thread
 
 import time
+import datetime
 
 import smtplib
 
-from secrets import HOST
+from .secrets import HOST, PORT, FROM, PASSWORD, LOGIN
+
 
 class SenderThread(Thread):
+# class SenderThread:
     '''need a description'''
+    host = HOST
+    time_to_send = None
 
     def __init__(self, *args, **kwargs):
-        Thread.__init__(self, *args, **kwargs)
-        self.name = name
+        Thread.__init__(self)
+        self.__host = kwargs.get('__host', HOST)
+        self.__port = kwargs.get('__port', PORT)
+        self.__from = kwargs.get('__from', FROM)
+        self.__pass = kwargs.get('__pass', PASSWORD)
+        self.__login = kwargs.get('__login', LOGIN)
+        self.__from = kwargs.get('__from', FROM)
+        self.rec_email = kwargs.get('rec_email')
+        self.mailtext = kwargs.get('mailtext')
+        self.sending_delay = kwargs.get('sending_delay', 0)
 
     def run(self):
-        server = smtplib.SMTP(self.host)
-        server.sendmail(self.FROM, self.TO, self.BODY)
+        print('Thread run')
+        time.sleep(self.sending_delay)
+        self.send_email(self)
+        print('Mail sended')
+
+    def send_email(self, *args, **kwargs):
+        server = smtplib.SMTP_SSL(self.__host, self.__port)
+        server.login(self.__login, self.__pass)
+        server.sendmail(
+            self.__from,
+            self.rec_email,
+            self.mailtext,
+        )
+        server.quit()
+
+
+def main():
+    sender = SenderThread(rec_email = '2100992@gmail.com', mailtext = 'testing mailtext', sending_delay = 10)
+    sender.start()
+    sender.is_alive
+
+
+if __name__ == "__main__":
+    main()
